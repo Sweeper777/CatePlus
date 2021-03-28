7cd1aec293aee34724183fa68f68810a19b1de4f
matsagad <matthew.u.young@gmail.com>
2021-03-28 13:39:42 +0800

    fixed all color inconsistencies with timetable

--- cateplus.js
+++ cateplus.js
@@ -71,12 +71,15 @@ function changeColor(targets, attr, oldColor, newColor) {
 changeColor("*", "color", "rgb(0, 0, 0)", "#FFFFFF");
 // change blue text to pink
 changeColor("*", "color", "rgb(0, 0, 255)", "#F391B1");
-// change red text to purple
+// change red/green text to purple
 changeColor("*", "color", "rgb(255, 0, 0)", "#BB86FC");
+changeColor("*", "color", "rgb(0, 128, 0)", "#BB86FC");
 // change blue accents to dark gray
 changeColor("td", "border-color", "rgb(0, 0, 255)", "#F39DBA");
 // change red accents to purple
 changeColor("td", "border-color", "rgb(255, 0, 0)", "#BB86FC");
+changeColor("td", "background-color", "rgb(244, 173, 183)", "#403030");
 // change gray accents to light gray
 changeColor("td", "background-color", "rgb(221, 221, 221)", "#403030");
+
 
