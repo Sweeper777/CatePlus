bbd045ba068d57a2761a50dccc2998a7071dc22b
Mulang Su <sumulang@gmail.com>
2021-03-28 18:37:47 +0800

    fixed notifications

--- notificationsBg.js
+++ notificationsBg.js
@@ -2,14 +2,44 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.notificationAction) {
             if (request.notificationAction == "clear") {
-                for (var id of request.notificationIds) {
-                    chrome.notifications.clear(id);
-                }
-                sendResponse({});
+                chrome.alarms.clearAll(sendResponse);
+                return true;
             } else if (request.notificationAction == "create") {
-                chrome.notifications.create(request.notificationId, request.notificationOptions, id => {});
-                sendResponse({});
+                chrome.alarms.create(request.notificationId, {
+                    when: request.notificationTime
+                });
+                sendResponse();
             }
         }
     }
 );
+
+
+function notificationId(deadline, key) {
+    return key + "-" + String(deadline.month) + "-" + String(deadline.day);
+}
+
+chrome.alarms.onAlarm.addListener(
+    function(alarm) {
+        var storageKey;
+        var deadlineType;
+        if (alarm.name.startsWith("examDeadlines")) {
+            storageKey = "examDeadlines";
+            deadlineType = "exam";
+        } else if (alarm.name.startsWith("homeworkDeadlines")) {
+            storageKey = "homeworkDeadlines";
+            deadlineType = "homework";
+        }
+        chrome.storage.local.get([storageKey], result => {
+            var storedDeadlines = result[storageKey];
+            var reached = storedDeadlines.filter(x => notificationId(x, storageKey) == alarm.name)[0];
+            
+            chrome.notifications.create(alarm.name, {
+                iconUrl: "icons/logo.png",
+                message: "You have " + String(reached.count) + " " + deadlineType + " deadline(s) tommorrow!",
+                title: "CATePlus",
+                type: "basic"
+            }, id => {});
+        })
+    }
+)
