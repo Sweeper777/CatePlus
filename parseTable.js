c86c049a0fdadf0d31bc015be3a910165776246b
tony <tony_t_liu@outlook.com>
2021-03-28 14:42:56 +0800

    added 'let's to parseTable.js

--- parseTable.js
+++ parseTable.js
@@ -1,40 +1,40 @@
-selectDate = function(){
-    htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(3)").children
-    dateArray = Array.from(htmlCollection).slice(1)
-    outputArray = []
-    for (i of dateArray){
-        value = i.innerText === "" ? 0 : parseInt(i.innerText)
+let selectDate = function(){
+    let htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(3)").children
+    let dateArray = Array.from(htmlCollection).slice(1)
+    let outputArray = []
+    for (let i of dateArray){
+        let value = i.innerText === "" ? 0 : parseInt(i.innerText)
         outputArray.push(value)
     }
     return outputArray
 }
 
-selectHw = function(){
-    htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(4)").children
-    hwArray = Array.from(htmlCollection).slice(2)
-    outputArray = []
+let selectHw = function(){
+    let htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(4)").children
+    let hwArray = Array.from(htmlCollection).slice(2)
+    let outputArray = []
     for (i of hwArray){
-        value = i.innerText === "" ? 0 : parseInt(i.innerText)
+        let value = i.innerText === "" ? 0 : parseInt(i.innerText)
         outputArray.push(value)
     }
     return outputArray
 }
 
-selectExam = function(){
-    htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(5)").children
-    examArray = Array.from(htmlCollection).slice(2)
-    outputArray = []
+let selectExam = function(){
+    let htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(5)").children
+    let examArray = Array.from(htmlCollection).slice(2)
+    let outputArray = []
     for (i of examArray){
-        value = i.innerText === "" ? 0 : parseInt(i.firstChild.firstElementChild.innerText)
+        let value = i.innerText === "" ? 0 : parseInt(i.firstChild.firstElementChild.innerText)
         outputArray.push(value)
     }
     return outputArray
 }
 
-selectMonth = function(){
-    htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(1)").children
-    monthArray = Array.from(htmlCollection).slice(1)
-    outputArray = []
+let selectMonth = function(){
+    let htmlCollection = $("body > table:nth-child(3) > tbody > tr:nth-child(1)").children
+    let monthArray = Array.from(htmlCollection).slice(1)
+    let outputArray = []
     for (i of monthArray){
         for (j = 0; j < i.colSpan; j++){
             outputArray.push(i.innerText)
@@ -44,11 +44,11 @@ selectMonth = function(){
 }
 
 // plug in DDL array (Exam or Homework)
-mapFromDDLToDate = function(DDLArray, dateArray, monthArray){
-    outputArray = []
+let mapFromDDLToDate = function(DDLArray, dateArray, monthArray){
+    let outputArray = []
     for (i = 0; i < DDLArray.length; i++){
         if (DDLArray[i] !== 0) {
-            obj = {
+            let obj = {
                 "numOfDDLs" : DDLArray[i],
                 "date" : dateArray[i],
                 "month" : monthArray[i]
