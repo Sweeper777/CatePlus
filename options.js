e01e91393ff750d87a1d5f2ed34c433e9602814d
Mulang Su <sumulang@gmail.com>
2021-03-28 19:16:17 +0800

    my last commit

--- options.js
+++ options.js
@@ -1,14 +1,18 @@
 
 function restoreOptions() {
-    chrome.storage.sync.get(["openSpecInNewTab"], result => {
+    chrome.storage.sync.get(["openSpecInNewTab", "customColorScheme", "customIcons"], result => {
         $("#open-in-new-tab").attr("checked", result.openSpecInNewTab);
+        $("#custom-color-scheme").attr("checked", result.customColorScheme);
+        $("#custom-icons").attr("checked", result.customIcons);
     });
 }
 
 $("#save").on("click", () => {
     chrome.storage.sync.set(
         {
-            openSpecInNewTab: $("#open-in-new-tab").is(":checked")
+            openSpecInNewTab: $("#open-in-new-tab").is(":checked"),
+            customColorScheme: $("#custom-color-scheme").is(":checked"),
+            customIcons: $("#custom-icons").is(":checked")
         },
         () => {
             $("#status").html("Options Saved!");
