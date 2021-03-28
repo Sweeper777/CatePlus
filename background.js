d1c3f5f8a3c6f86d8bda0e3e2855c71d92956e82
tony <tony_t_liu@outlook.com>
2021-03-28 18:12:26 +0800

    import template to page

--- background.js
+++ background.js
@@ -1,20 +1,10 @@
 var options;
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if(request.cmd == "read_file") {
-        $.ajax({
-            url: chrome.extension.getURL("template.html"),
-            dataType: "html",
-            success: sendResponse
-        });
-    }
-})
-
 chrome.storage.sync.get(["openSpecInNewTab"], result => {
     options = result;
 })
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
+chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (var key in changes) {
         console.log(key);
         if (["openSpecInNewTab"].indexOf(key) >= 0) {
@@ -24,17 +14,35 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 });
 
 chrome.webRequest.onHeadersReceived.addListener(
-    function(details) { 
+    function (details) {
         if (!options.openSpecInNewTab) {
-            return {responseHeaders: details.responseHeaders};  
+            return { responseHeaders: details.responseHeaders };
         }
         details.responseHeaders.forEach((header, i, a) => {
             if (header.name == "Content-Disposition") {
                 details.responseHeaders[i].value = "inline";
             }
         });
-        return {responseHeaders: details.responseHeaders}; 
+        return { responseHeaders: details.responseHeaders };
     },
-    {urls: ["https://cate.doc.ic.ac.uk/showfile.cgi*"]},
+    { urls: ["https://cate.doc.ic.ac.uk/showfile.cgi*"] },
     ["responseHeaders", "blocking"]
+);
+
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//       if (request.greeting == "hello")
+//         sendResponse({farewell: "goodbye"});
+//     }
+//   );
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.greeting == "hello") {
+            console.log("I am here!");
+            $.get(chrome.extension.getURL("template.html"),
+                (data) => { sendResponse(data) });
+            return true;
+        }
+    }
 );
--- swapMainPage.js
+++ swapMainPage.js
@@ -1,6 +1,4 @@
 let getInformation = function () {
-
-
     let texts = $('b').slice(0, 9);
     let obj = {
         name: $(texts[0]).text(),
@@ -11,35 +9,50 @@ let getInformation = function () {
         category: $(texts[5]).text(),
         email: $(texts[6]).text(),
         pTutor: $(texts[7]).text(),
-        timetablePeriod: $($(texts[8]).parent().parent().parent().find(":first-child")[1]).val(),
-        year: $('font').slice(0).html().substring(0, 4),
-        moduleCode: category.split(" ")[3],
-        // the appropriate time-table link:
-        timetableLink: "https://cate.doc.ic.ac.uk/timetable.cgi?keyt=" + year + ":" + timetablePeriod + ":" + moduleCode + ":" + login
+        timetablePeriod: $($(texts[8]).parent().parent().parent().find(" :first-child")[1]).val(),
+        year: $('font').slice(0).html().substring(0, 4)
     }
+
+    obj.moduleCode = obj.category.split(" ")[3];
+    // the appropriate time-table link :
+    obj.timetableLink = "https://cate.doc.ic.ac.uk/timetable.cgi?keyt=" + obj.year + ":" + obj.timetablePeriod + ":" + obj.moduleCode + ":" + obj.login;
     return obj;
 }
+
+let generateButton = function (link, id, text) {
+    var button = `<button type = "button" class = "btn" id = ${id}
+                 onclick = "window.location.href = ${link}> ${text}</button>`;
+    console.log(button)
+    return button;
+}
+
 // after swap the whole thing
 let main = function () {
-    chrome.extension.sendRequest({ cmd: "read_file" }, function (html) {
-        document.write(html)
+    let information = getInformation()
+    console.log(information)
+    let r;
+    chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
+        r = response;
+        console.log(r)
+        $("html").html(r);
+        afterwards(information)
     });
+
 }
 // plugin the variables
 
 let afterwards = function (data) {
 
-    $("#name").replaceWith(data.name)
-    $("#short code").replaceWith(data.shortCode)
-    $("#status").replaceWith(data.status)
-    $("#cid").replaceWith(data.cid)
-    $("department").replaceWith(data.department)
-    $("category class").replaceWith(data.categoryClass)
-    $("email").replaceWith(data.email)
-    $("personal tutor").replaceWith(data.personalTutor)
+    let button = generateButton(data.timetableLink, "timetable button", "timetable")
 
-    $("#photo").replaceWith(data.photo)
-
-    $("#link to timetable button").replaceWith(data.timeTableButton)
-
+    $("#name").append(data.name)
+    $("#status").append(data.status)
+    $("#cid").append(data.cid)
+    $("#department").append(data.dept)
+    $("#category_class").append(data.category)
+    $("#email").append(data.email)
+    $("#personal_tutor").append(data.pTutor)
+    $("#timetable_button").on("click", () => { window.open(data.timetableLink) })
 }
+
+main()
