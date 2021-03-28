ffa6cf3d09e748b27f987f740845a16d9cb76f12
matsagad <matthew.u.young@gmail.com>
2021-03-28 13:13:10 +0800

    Assignments for unassessed and assessed modules.

--- cateplus.js
+++ cateplus.js
@@ -1,7 +1,8 @@
 
 // if on main page
+var title = $('title').text();
 
-if ($('title').text().substring(0,4) == "CATe") {
+if (title.substring(0,4) == "CATe") {
 	// extract necessary information
 
 	let texts = $('b').slice(0,9);
@@ -16,21 +17,51 @@ if ($('title').text().substring(0,4) == "CATe") {
 	const pTutor = $(texts[7]).text();
 	const timetablePeriod = $($(texts[8]).parent().parent().parent().find(":first-child")[1]).val();
 	const year = $('font').slice(0).html().substring(0,4);
+	const moduleCode = category.split(" ")[3];
 
 	// the appropriate time-table link:
-	const timetableLink = "https://cate.doc.ic.ac.uk/timetable.cgi?keyt=" + year + ":" + timetablePeriod + ":j1:" + login;
+	const timetableLink = "https://cate.doc.ic.ac.uk/timetable.cgi?keyt=" + year + ":" + timetablePeriod + ":" + moduleCode + ":" + login;
 	// console.log(timetableLink)
 
 	// for tony: use this info for your afterwards.js
 }
 
+if (title.substring(title.length - 9) == "Timetable") {
+
+	// assign classes for each group
+
+	const unassessed = retrieveMatchingCSS("td", "background-color", "rgb(255, 255, 255)");
+	$(unassessed).addClass("unassessed");
+
+	const unassessedRequired = retrieveMatchingCSS("td", "background-color", "rgb(205, 205, 205)");
+	$(unassessedRequired).addClass("unassessedRequired");
+
+	const assessedIndividual = retrieveMatchingCSS("td", "background-color", "rgb(204, 255, 204)");
+	$(assessedIndividual).addClass("assessedIndividual");
+
+	const assessedGroup = retrieveMatchingCSS("td", "background-color", "rgb(240, 204, 240)");
+	$(assessedGroup).addClass("assessedGroup");
+
+
+	// make lines pink
+	
+	$('td').filter(function(){
+		return ($(this).css("background-color") == "rgb(142, 249, 249)");
+	}).css("background-color", "#F39DBA");
+
+}
+
 
 // naive way to change all black and blue to white and pink
 
+function retrieveMatchingCSS(targets, attr, oldValue){
+	return $(targets).filter(function(){
+		return ($(this).css(attr) == oldValue);
+	});
+}
+
 function changeColor(attr, oldColor, newColor) {
-	$('*').filter(function(){
-		return ($(this).css(attr) == oldColor)
-	}).css(attr, newColor);
+	$(retrieveMatchingCSS("*", attr, oldColor)).css(attr, newColor);
 }
 
 // change black text to white
@@ -41,3 +72,7 @@ changeColor("color", "rgb(0, 0, 255)", "#F391B1");
 
 // change blue accents to dark gray
 changeColor("border-color", "rgb(0, 0, 255)", "#BB86FC");
+
+
+
+
