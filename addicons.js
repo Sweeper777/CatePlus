ecc221cecdf12b4aeee6cdfea1cd2a8d1c19c3d4
matsagad <matthew.u.young@gmail.com>
2021-03-28 18:27:58 +0800

    Simplified code

--- addicons.js
+++ addicons.js
@@ -22,56 +22,24 @@ $("head").find("link").filter(function(){
 
 var title = $('title').text();
 
-// main page
-if (title.substring(0,4) == "CATe" && title != "CATe - Grading Scheme") {
-    //logo
-    $("body > form > table > tbody > tr:nth-child(1) > td:nth-child(1) > h2 > img").attr("src",logoURL).height(20);
-
-    //left "right arrow"
-    $("body > form > table > tbody > tr:nth-child(2) > td:nth-child(1) > ul:nth-child(2) > img").attr("src",rightArrowURL).height(20);
-
-    //left info
-    $("body > form > table > tbody > tr:nth-child(2) > td:nth-child(1) > ul:nth-child(2) > a > img").attr("src",infoURL).height(20);
-
-    
-    //mainpage left table arrow
-    $("body > form > table > tbody > tr:nth-child(2) > td:nth-child(1) > ul:nth-child(3) > form > table > tbody > tr > td:nth-child(3) > table > tbody > tr > td > table > tbody > tr:nth-child(1) > th > img").attr("src",downArrowURL).height(15);
-    //mainpage first right down arrow
-    $("body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul > table > tbody > tr:nth-child(1) > th:nth-child(3) > img").attr("src",downArrowURL).height(15);
-    //mainpage second right down arrow
-    $("body > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > ul > table > tbody > tr:nth-child(1) > th:nth-child(3) > img").attr("src",downArrowURL).height(15);
-    
-    //right grading icon
-    $("body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul > table > tbody > tr:nth-child(3) > td:nth-child(3) > a > img").attr("src", gradingURL).height(28);
-    
-    //right late icon
-    $("body > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > ul > table > tbody > tr:nth-child(4) > td:nth-child(3) > a > img").attr("src", lateURL).height(12);
-
-    //right email icon
-    $("body > form > table > tbody > tr:nth-child(3) > td:nth-child(2) > ul > table > tbody > tr:nth-child(2) > td:nth-child(3) > font > a > img").attr("src",emailURL).height(30);
+function replaceImage(oldSrc, newSrc, height){
+    $("img").filter(function(){
+        return $(this).attr("src") == oldSrc;
+    }).attr("src", newSrc).height(height);
 }
 
 // timetable
 if (title.includes("Timetable")) {
-    //left -1 period
-    $("body > center > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(1) > td:nth-child(1) > a > img").attr("src",leftArrowSingleURL).height(20);
 
-    //left -2 period
-    $("body > center > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(1) > a:nth-child(1) > img").attr("src",leftArrowSingleURL).height(20);
-    $("body > center > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(2) > td:nth-child(1) > a:nth-child(2) > img").attr("src",leftArrowSingleURL).height(20);
+    replaceImage("icons/arrowright.gif", rightArrowSingleURL, 20);
+    replaceImage("icons/arrowleft.gif", leftArrowSingleURL, 20);
+    replaceImage("icons/arrowredright.gif", rightArrowURL, 20);
+    replaceImage("icons/info.gif", infoURL, 20);
 
-    //right +1 period
-    $("body > center > table > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(2) > a > img").attr("src",rightArrowSingleURL).height(20);
-    
-    //right +2 period
-    $("body > center > table > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > a:nth-child(2) > img").attr("src",rightArrowSingleURL).height(20);
-    $("body > center > table > tbody > tr > td:nth-child(3) > table > tbody > tr:nth-child(2) > td:nth-child(2) > a:nth-child(1) > img").attr("src",rightArrowSingleURL).height(20);
-
-    //right arrow
-    $("body > center > ul > img").attr("src",rightArrowURL).height(20);
-
-    //info
-    $("body > center > ul > a > img").attr("src",infoURL).height(20);
+    $("img").filter(function(){
+        var src = $(this).attr("src");
+        return (src == "icons/deadline_small.gif") || (src == "icons/tests_small.gif");
+    }).attr("src", rightArrowSingleURL).height(20);
 }
 
 // grading scheme
@@ -81,13 +49,9 @@ if (title.includes("Grading Scheme")) {
 
 // projects portal
 if (title.includes("Projects Portal")) {
-    $("img").filter(function(){
-        return $(this).attr("src") == "icons/info.gif";
-    }).attr("src", infoURL).height(20);
-
-    $("img").filter(function(){
-        return $(this).attr("src") == "icons/admin.gif";
-    }).attr("src", adminURL).height(20);
+    
+    replaceImage("icons/info.gif", infoURL, 20);
+    replaceImage("icons/admin.gif", adminURL, 20);
 }
 
 
